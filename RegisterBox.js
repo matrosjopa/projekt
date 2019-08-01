76b9387b819c9d4ed589ab03a138029480363007
matrosjopa <52970941+matrosjopa@users.noreply.github.com>
2019-08-01 19:48:52 +0300

    Add files via upload

---
+++ RegisterBox.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+export class RegisterBox extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state= {};
+    }
+
+    render () {
+        return (
+            <div>Register</div>
+        )
+    }
+ }
